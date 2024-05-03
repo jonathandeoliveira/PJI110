23934fbea8fd25bd94a350f227492fffb0ef49bd
vanderson <vanderson3e@gmail.com>
2024-05-02 22:59:00 -0300

    Implementação de alteração cadastral

--- dona_jo_library/users/urls.py
+++ dona_jo_library/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("cadastrar/", views.cadastrar, name="cadastrar"),
     path("validate_user/", views.validate_user, name="validate_user"),
     path("validates_login/", views.validates_login, name="validates_login"),
+    path("alterar_cadastro/", views.update_user, name="update_user")
 ]
