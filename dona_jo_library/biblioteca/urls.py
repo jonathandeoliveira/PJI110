744c64103f6cd28517fc445b0982d292694e18b0
unknown <gusssantos012@gmail.com>
2024-05-13 16:22:16 -0300

    "Atualização de cadastro de livro"

--- dona_jo_library/biblioteca/urls.py
+++ dona_jo_library/biblioteca/urls.py
@@ -5,8 +5,14 @@ from users.models import UserProfile, UserTypes
 urlpatterns = [
     path("cadastrar/", views.cadastrar, name="cadastrar"),
     path("home/", views.home, name="home"),
+
     path("cadastrar-livro/", views.cadastrar_livro, name="cadastrar_livro"),
-    path("valida_cadastro_livro/", views.valida_cadastro_livro, name='valida_cadastro_livro'),
+    path("valida_cadastro_livro/", views.valida_cadastro_livro, 
+         name='valida_cadastro_livro'),
+    path("atualiza-livro/", views.atualiza_livro, name="atualiza_livro"),
+    path("atualiza_cadastro_livro/", views.atualiza_cadastro_livro,
+         name="atualiza_cadastro_livro"),
+
 
     #path("home/", views.home, name="home"),
     path("emprestimos/", views.emprestimos, name="emprestimos"),
