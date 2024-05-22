79811a745519be14dd5bde5c20a228dfaebdcd20
jonathandeoliveira <jonathan_oliveira1997@hotmail.com>
2024-05-21 23:32:04 -0300

    mais melhorias no front: estilizacao de botoes

--- dona_jo_library/users/urls.py
+++ dona_jo_library/users/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path("sair/", views.sair, name="sair"),
     path("cadastrar/", views.cadastrar, name="cadastrar"),
     path("validate_user/", views.validate_user, name="validate_user"),
-    path("alterar_cadastro/", views.update_user, name="update_user"),
+    path("alterar_cadastro/", views.alterar_cadastro, name="alterar_cadastro"),
+    path("update_user/", views.update_user, name="update_user"),
     path("validates_login/", views.validates_login, name="validates_login"), # type: ignore
     path('minha-conta/', views.myaccount, name="myaccount")
 ]
